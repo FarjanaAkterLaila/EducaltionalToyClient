358a593513122abdecdf01862e0d2901e464710a
FarjanaAkterLaila <farjanaakter7275@gmail.com>
2023-05-21 23:39:36 +0600

    update

--- src/Componant/Subcatrgory/Subcategory.jsx
+++ src/Componant/Subcatrgory/Subcategory.jsx
@@ -20,36 +20,40 @@ const Subcategory = () => {
     return (
          <div>
         <h1 className="iteam fs-2 text-center mt-5 p-5">Shop by category</h1>
-        <div className=" text-center">
-          <div className="text-center w-100 m-auto">
-            <div className="d-flex justify-content-center align-items-center">
-              <div
-                onClick={() => handleTabClick("Science")}
-                className={`tab iteam fs-4 Science ${
-                  activeTab == "Science" ? " bg-secondary text-white" : ""
-                }`}
-              >
-                Science Toys
-              </div>
-              <div
-                onClick={() => handleTabClick("Engineering")}
-                className={`tab iteam fs-4 Engineering ${
-                  activeTab == "Engineering" ? " bg-secondary text-white" : ""
-                }`}
-              >
-                Engineering toys
-              </div>
-              <div
-                onClick={() => handleTabClick("Language")}
-                className={`tab iteam fs-4 Language ${
-                  activeTab == "Language" ? " bg-secondary text-white" : ""
-                }`}
-              >
-               Language Toys
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="text-center">
+        <div className="row row-cols-1 row-cols-md-3 m-auto">
+    <div className="col">
+      <div
+        onClick={() => handleTabClick("Science")}
+        className={`tab iteam fs-4 Science ${
+          activeTab === "Science" ? "bg-secondary text-white" : ""
+        }`}
+      >
+        Science Toys
+      </div>
+    </div>
+    <div className="col">
+      <div
+        onClick={() => handleTabClick("Engineering")}
+        className={`tab iteam fs-4 Engineering ${
+          activeTab === "Engineering" ? "bg-secondary text-white" : ""
+        }`}
+      >
+        Engineering Toys
+      </div>
+    </div>
+    <div className="col">
+      <div
+        onClick={() => handleTabClick("Language")}
+        className={`tab iteam fs-4 Language ${
+          activeTab === "Language" ? "bg-secondary text-white" : ""
+        }`}
+      >
+        Language Toys
+      </div>
+    </div>
+  </div>
+</div>
         <div className="row row-cols-1 row-cols-md-2 g-1 mx-5 mt-5">
           {jobs?.map((job) => (
             <Catergorydetails 
